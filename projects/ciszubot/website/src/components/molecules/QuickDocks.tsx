83f4d01931c3434b084bf0efed69310f6daf1d9e
CiszukoAntony <fplayersoffcial@gmail.com>
2026-09-16 18:16:56 -0400

    fix: eliminar items duplicados en QuickDocks de ciszu, ciszubot y ciszukoantony

--- projects/ciszubot/website/src/components/molecules/QuickDocks.tsx
+++ projects/ciszubot/website/src/components/molecules/QuickDocks.tsx
@@ -103,17 +103,6 @@ const DOCK_ITEMS: DockItem[] = [
     ),
   },
   {
-    label: 'Commands',
-    href: '/commands',
-    color: 'blue',
-    icon: (
-      <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={1.8} strokeLinecap="round" strokeLinejoin="round" className="w-6 h-6">
-        <polyline points="4 17 10 11 4 5" />
-        <line x1="12" y1="19" x2="20" y2="19" />
-      </svg>
-    ),
-  },
-  {
     label: 'Information',
     href: '/information',
     color: 'purple',
